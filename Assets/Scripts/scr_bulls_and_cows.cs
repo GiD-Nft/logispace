1ab66d0eb8e851067f305933905d4cd9aa1fd495
bimaxd <35865847+bimaxd@users.noreply.github.com>
2018-12-17 13:23:19 +0500

    bK

--- Assets/Scripts/scr_bulls_and_cows.cs
+++ Assets/Scripts/scr_bulls_and_cows.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 
 public class scr_bulls_and_cows : MonoBehaviour {
-    public string textField = "";
+    public static string textField = "";
     public static string textAreaString = "Компьютер";
     public static ArrayList list = new ArrayList();
     public static int count = 0;
@@ -21,6 +21,9 @@ public class scr_bulls_and_cows : MonoBehaviour {
     public bool check1 = true;
     string number;
     public static string textAreaComp = "Игрок";
+    Vector2 scrollPosition;
+    Vector2 scrollPosition2;
+
 
     // Use this for initialization
     void Start () 
@@ -39,24 +42,87 @@ public class scr_bulls_and_cows : MonoBehaviour {
 
     void OnGUI()
     {
+        GUILayout.BeginArea(new Rect(640, 25, 300, 280));
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(300), GUILayout.Height(280));
+        GUILayout.TextArea(textAreaString);
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(10, 25, 300, 280));
+        scrollPosition2 = GUILayout.BeginScrollView(scrollPosition2, GUILayout.Width(300), GUILayout.Height(280));
+        GUILayout.TextArea(textAreaComp);
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+
+        GUI.TextField(new Rect(444, 10, 55, 30), textField);
+
+        if (GUI.Button(new Rect(45, 310, 50, 50), "0")) {
+            writeToTExtFiled("0");
+        }
+        if (GUI.Button(new Rect(115, 310, 50, 50), "1"))
+        {
+            writeToTExtFiled("1");
+        }
+        if (GUI.Button(new Rect(185, 310, 50, 50), "2"))
+        {
+            writeToTExtFiled("2");
+        }
+        if (GUI.Button(new Rect(255, 310, 50, 50), "3"))
+        {
+            writeToTExtFiled("3");
+        }
+        if (GUI.Button(new Rect(325, 310, 50, 50), "4"))
+        {
+            writeToTExtFiled("4");
+        }
+        if (GUI.Button(new Rect(395, 310, 50, 50), "5"))
+        {
+            writeToTExtFiled("5");
+        }
+        if (GUI.Button(new Rect(465, 310, 50, 50), "6"))
+        {
+            writeToTExtFiled("6");
+        }
+        if (GUI.Button(new Rect(535, 310, 50, 50), "7"))
+        {
+            writeToTExtFiled("7");
+        }
+        if (GUI.Button(new Rect(605, 310, 50, 50), "8"))
+        {
+            writeToTExtFiled("8");
+        }
+        if (GUI.Button(new Rect(675, 310, 50, 50), "9"))
+        {
+            writeToTExtFiled("9");
+        }
+        if (GUI.Button(new Rect(745, 310, 140, 50), "backspace"))
+        {
+            if(textField!=""&&textField!=null)
+            textField = textField.Substring(0, textField.Length - 1);
+        }
+
         
-        
-    textField = GUI.TextField(new Rect(444, 10, 55, 30), textField);
-    GUI.skin.textField.fontSize = 20;
-        if (GUI.Button(new Rect(425, 250, 100, 30), "Выйти")) {
+        GUI.skin.textField.fontSize = 20;
+        if (GUI.Button(new Rect(425, 250, 100, 30), "Выйти"))
+        {
             Control.SpaceObjectsActivate(true);
             Control.setLandCameraSize(false);
             Destroy(GameObject.Find("bullsAndCowsObj"));
         }
 
-        textAreaString = GUI.TextArea(new Rect(640, 25, 300, 280), textAreaString);
-        textAreaComp = GUI.TextArea(new Rect(10, 25, 300, 280), textAreaComp);
-        if (check1 && GUI.Button(new Rect(425, 100, 100, 30), "Задать число")) {
-            writeToUser("Ваше число теперь = " + textField);
-            number = textField;
-            check1 = false;
-            textField = "";
-            writeToUser("Напишите 4 значное число в поле по центру, и нажмите угадать чтоб сделать ход");
+        if (check1 && GUI.Button(new Rect(425, 100, 100, 30), "Задать число"))
+        {
+            if (textField != "" && textField != null && textField.Length == 4 )
+            {
+                writeToUser("Ваше число теперь = " + textField);
+                number = textField;
+                check1 = false;
+                textField = "";
+                writeToUser("Напишите 4 значное число в поле по центру, и нажмите угадать чтоб сделать ход");
+            }
+            else {
+                writeToUser("Вы ввели что-то не правильно");
+            }
 
         }
 
@@ -69,11 +135,14 @@ public class scr_bulls_and_cows : MonoBehaviour {
             ans[1] = (ansint / 100) % 10;
             ans[2] = (ansint / 10) % 10;
             ans[3] = ansint % 10;
-            UserGame(ans, textField); // тут заменить Console.ReadLine на число пользоваеля которое он вводит чтоб угадать
-            Game(number); // Тут вызывается хо компа
+            if (UserGame(ans, textField))
+            {
+                Game(number);
+            }// Тут вызывается хо компа
             textField = "";
         }
     }
+
     public static void writeToCom(string text) {
         textAreaString = textAreaString + "\n" + text;
     }
@@ -81,6 +150,10 @@ public class scr_bulls_and_cows : MonoBehaviour {
     public static void writeToUser(string text)
     {
         textAreaComp = textAreaComp + "\n" + text;
+    }
+
+    public static void writeToTExtFiled(string text) {
+        textField = textField + text;
     }
 
     public static ArrayList GenerateList()
@@ -146,7 +219,7 @@ public class scr_bulls_and_cows : MonoBehaviour {
             }
             else if (a == 4 && b == 0)
             {
-                writeToCom("Твое число = " + compGuess);
+                writeToCom("Вы проиграли, враги теперь могут свободно уничтожить вашу планету " + compGuess);
                 return true;
             }
             return true;
@@ -199,7 +272,7 @@ public class scr_bulls_and_cows : MonoBehaviour {
         else
         {
             writeToUser((int)ab[0]+ " быков, и " + (int)ab[1] + " коров");
-            return false;
+            return true;
         }
     }
 
